9644f9e7f22b274cb4e3f5183f63f3bd00ba9092
Frankmona <frankoguche1@gmail.com>
2023-04-13 17:38:11 +0100

    learning

--- javascript-fundamentals/main.js
+++ javascript-fundamentals/main.js
@@ -8,7 +8,29 @@
 // console.log($people);
 
 // string concatenation
-const firstName = "frank";
-const lastName = "oguche";
-const fullName = firstName + " " + lastName;
-console.log("Hello my name is " + fullName);
+// const firstName = "frank";
+// const lastName = "oguche";
+// const fullName = firstName + " " + lastName;
+// console.log("Hello my name is " + fullName);
+
+// const website = "udemy";
+// const url = "www." + website + ".com";
+// console.log(url);
+
+// let number = 23;
+// number += 5;
+
+// let number = 23;
+// number++;
+
+// console.log(number);
+
+// strings
+//  type of use to tell what type is the data type
+// const text = "frank";
+// console.log(typeof text);
+
+// array
+
+const friends = ["frank", "mona", "oguche", 21, "mercy"];
+console.log(friends[4]);
