1ab51fde55586d30d3981bfa47b5f87a80de3520
Frankmona <frankoguche1@gmail.com>
2023-04-14 17:45:53 +0100

    learning

--- javascript-fundamentals/main.js
+++ javascript-fundamentals/main.js
@@ -32,5 +32,76 @@
 
 // array
 
-const friends = ["frank", "mona", "oguche", 21, "mercy"];
-console.log(friends[4]);
+// const friends = ["frank", "mona", "oguche", 21, "mercy"];
+// console.log(friends[4]);
+
+// let bestfriend = friends[2];
+// console.log(bestfriend);
+
+// function
+
+// helps to reduce repeated of code
+// function greet(name) {
+//   console.log("Hello " + name);
+// }
+// greet("sussy");
+// greet("frank");
+// greet("mona");
+
+// using 1inch = 2.54cm
+// function calculate(value) {
+//   const newValue = value * 2.54;
+//   console.log("The value is " + value * 2.54 + " cm");
+//   //   return value * 2.54;
+//   return newValue;
+// }
+// const wallWidth = 10;
+// const wallHeight = 5;
+// const width = calculate(wallWidth);
+// const height = calculate(wallHeight);
+// const dimension = [width * height];
+// console.log(dimension + "cm");
+
+// function declaration
+
+// function addvalues(num1, num2) {
+//   return num1 + num2;
+// }
+
+// const firstvalue = addvalues(3, 5);
+// const secondvalue = addvalues(1, 3);
+
+// // function expression
+// const add = function addvalues(num1, num2) {
+//   return num1 + num2;
+// };
+
+// // const thirdvalue = add(5, 4);
+// const values = [firstvalue, secondvalue, add(5, 6)];
+// console.log(values);
+
+// objects;
+// objects show the properties e.t.c  e.g using a car as an object its properties  having red color been toyota and all that
+
+// const person = {
+//   firstName: "frank",
+//   lastName: "oguche",
+//   age: 50,
+//   education: false,
+//   married: true,
+//   sibilings: ["mbape", "paulo", "pogba", "kylian"],
+//   greeting: function () {
+//     console.log("hello welcome to my page ");
+//   },
+// };
+// const age = person.age;
+// console.log(age);
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.sibilings[2]);
+// person.greeting();
+
+let x = 10;
+y = 5;
+const isValid = x > 8 && 8 > y;
+console.log(isValid);
