ce5acfbba50028984deb78526db39c4aff9ffb05
Frankmona <frankoguche1@gmail.com>
2023-05-01 08:43:42 +0100

    updated footer

--- javascript-fundamentals/main.js
+++ javascript-fundamentals/main.js
@@ -861,3 +861,19 @@
 // event propagation -- orders the event are fired
 // event bubbling -- clicked element first then bubbles up -- default
 // event capturing -- fires at the rrot and fires until reaches target
+
+const container = document.querySelector(".container");
+const list = document.querySelector(".listItems");
+
+function showBubling(event) {
+  console.log(`current target`, event.currentTarget);
+  console.log(`target`, event.target);
+
+  if (event.target.classList.contains("link")) {
+    console.log(`Mona you clicked it!`);
+  } else {
+    console.log(`click again`);
+  }
+}
+container.addEventListener("click", showBubling);
+list.addEventListener("click", showBubling);
